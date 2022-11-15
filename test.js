06613aa97667283df24199655c02cd1aaf049dc6
unknown <sadmanhoque@yahoo.com>
2022-11-15 10:40:58 -0400

    git the randomizer working on the test script

--- test.js
+++ test.js
@@ -1,23 +1,33 @@
 require('dotenv').config()
 var axios = require('axios');
 
+//random number generator
+function between(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
+var rando = between(0, 21)
+//console.log(rando)
+
 //Inputs for testing the API
-var input = 'pizza'
+var input = 'sandwich'
 var location = '44.666070, -63.657702'
 var radius = '1000'
 
+//config for the axios function
 var config = {
     method: 'get',
     url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${input}&location=${location}&radius=${radius}&key=${process.env.GOOGLE_MAPS_API_KEY}`,
     headers: { }
   };
 
+//this is where the real stuff happens
   axios(config)
   .then((response) =>{
     jsonData = JSON.stringify(response.data)
     jsonParsed = JSON.parse(jsonData)
-    //console.log(JSON.stringify(response.data));
-    console.log(jsonParsed.results[0].name)
+    console.log(jsonParsed.results[rando].name)
 
   })
   .catch((error) => {
