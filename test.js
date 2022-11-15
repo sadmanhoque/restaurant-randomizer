a7c730b80fce63a1f9207f7926c2bc69d9c0819d
unknown <sadmanhoque@yahoo.com>
2022-11-15 10:23:17 -0400

    got the backend function in test to retrieve and parse json

--- test.js
+++ test.js
@@ -1,24 +1,8 @@
 require('dotenv').config()
 var axios = require('axios');
 
-//const {Client} = require("@googlemaps/google-maps-services-js");
-// const client = new Client({});
-// client
-//   .ElevationService({
-//     params: {
-//       locations: [{ lat: 45, lng: -110 }],
-//       key: process.env.GOOGLE_MAPS_API_KEY,
-//     },
-//     timeout: 1000, // milliseconds
-//   })
-//   .then((r) => {
-//     console.log(r.data.results[0].elevation);
-//   })
-//   .catch((e) => {
-//     console.log(e.response.data.error_message);
-//   });
-
-var input = 'cake'
+//Inputs for testing the API
+var input = 'pizza'
 var location = '44.666070, -63.657702'
 var radius = '1000'
 
@@ -27,11 +11,16 @@ var config = {
     url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${input}&location=${location}&radius=${radius}&key=${process.env.GOOGLE_MAPS_API_KEY}`,
     headers: { }
   };
-  
+
   axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
+  .then((response) =>{
+    jsonData = JSON.stringify(response.data)
+    jsonParsed = JSON.parse(jsonData)
+    //console.log(JSON.stringify(response.data));
+    console.log(jsonParsed.results[0].name)
+
   })
-  .catch(function (error) {
+  .catch((error) => {
     console.log(error);
   });
+  
