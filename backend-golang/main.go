406f372bed875386cfd2991ef29958b3947341d9
unknown <sadmanhoque@yahoo.com>
2023-08-28 23:37:37 -0300

    small updates to the function receiving the seach terms and  returning the results

--- backend-golang/main.go
+++ backend-golang/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"errors"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,14 +13,14 @@ import (
 
 func searchByItem(c *gin.Context) {
 	item := c.Param("item")
-	book, err := PerformGetRequest(item)
+	result, err := PerformGetRequest(item)
 
 	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Something went wrong"})
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, book)
+	c.IndentedJSON(http.StatusOK, result)
 }
 
 func corsMiddleware() gin.HandlerFunc {
@@ -38,32 +38,35 @@ func corsMiddleware() gin.HandlerFunc {
 	}
 }
 
-func PerformGetRequest(item string) {
+func PerformGetRequest(item string) (*result, error) {
 
 	envErr := godotenv.Load(".env")
 	if envErr != nil {
 		fmt.Println("Could not load .env file")
 		os.Exit(1)
+		return nil, errors.New("Problem retreiving API key")
 	}
 
 	var apiKey = os.Getenv("API_KEY")
-
 	var myurl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + item + "&location=44.666070,-63.657702&radius=1000&key=" + apiKey
 
 	response, err := http.Get(myurl)
 
 	if err != nil {
-		panic(err)
+		//panic(err)
+		return nil, errors.New("Problem with executing Google maps API")
 	}
 
 	defer response.Body.Close()
 
-	fmt.Println("Status code: ", response.StatusCode)
+	//fmt.Println("Status code: ", response.StatusCode)
 
 	content, _ := io.ReadAll(response.Body)
 
 	fmt.Println(string(content))
+	result := string(content)
 
+	return result
 }
 
 func main() {
